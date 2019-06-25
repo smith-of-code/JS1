2e460d2c21fa0c360d39245148ed32666f6993c6
andreycoder <Sdamvse$vbank#1>
2019-06-25 11:54:02 +0300

    ДЗ 6 сделал

--- lesson6/shop/js/main.js
+++ lesson6/shop/js/main.js
@@ -14,7 +14,8 @@ function generProducts() {
 function createProduct(index) {
     return {
         name: NAMES[index],
-        price: PRICES[index]
+        price: PRICES[index],
+        count : 1
     }
 }
 
@@ -36,12 +37,32 @@ function productsRender() {
 function handlAddToCart() {
 let item = event.target;
 if (item.classList.contains('buy-btn')){
-    CART.push(PRODUCTS[item.dataset.btn]);
+    if (CART.indexOf(PRODUCTS[item.dataset.btn]) === -1){
+        CART.push(PRODUCTS[item.dataset.btn]);
+    }else  {
+        CART[CART.indexOf(PRODUCTS[item.dataset.btn])].count++;
+    }
+
     cartRender();
 }
 }
 
+document.querySelector('.btn-cart').addEventListener('click', handlCloseOpenCart);
+
+
+function handlCloseOpenCart(){
+    let $cartBlock = document.querySelector('.cart-block');
+    let key = event.target;
+    if(key.classList.contains('btn-cart')){
+        $cartBlock.classList.toggle('invisible');
+    }
+}
 function cartRender() {
+    let cartSum = 0;
+    for (let i = 0;i<CART.length;i++){
+        cartSum += CART[i].price * CART[i].count
+    }
+
     let $cartList = document.querySelector('.cart-block');
     $cartList.innerHTML = '';
     for (let i = 0;i<CART.length;i++){
@@ -51,15 +72,20 @@ function cartRender() {
                 <div class="product-bio">
                     <div class="product-desc">
                         <p class="product-title">${CART[i].name}</p>
-                        <p class="product-single-price">${CART[i].price}</p>
+                        <p class="product-quantity">Кол-во: ${CART[i].count}</p>
+                        <p class="product-single-price">${CART[i].price} рублей</p>
                     </div>
                 </div>
                 <div class="right-block">
+                    <p class="product-price">${CART[i].count * CART[i].price }</p>
                     <button class="del-btn">×</button>
                 </div>
             </div>
         `)
     }
+    $cartList.insertAdjacentHTML("beforeend",`
+    <div class="cart-sum">Товаров на сумму ${cartSum} рублей</div>
+    `)
 }
 generProducts();
 
